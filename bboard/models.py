f4a599a57bfaf01631668e02d11f2273a50f30fe
Бахытжан Бекарыс <@step.kz>
2023-11-23 21:35:14 +0600

    lesson_06 files

--- bboard/models.py
+++ bboard/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
+is_all_posts_passive = True
+
+
+def is_active_default():
+    return is_all_posts_passive
+
 
 class Rubric(models.Model):
-    name = models.CharField(max_length=20, db_index=True, verbose_name="Название")
+    name = models.CharField(max_length=20, db_index=True, verbose_name="Название", unique=True)
 
     def __str__(self):
         return f'{self.name}'
@@ -14,11 +20,45 @@ class Rubric(models.Model):
 
 
 class Bb(models.Model):
+    # class Kinds(models.TextChoices):
+    #     BUY = 'b', 'Куплю'
+    #     SELL = 's', 'Продам'
+    #
+    # kind = models.CharField(max_length=1, choices=Kinds.choices, default=Kinds.SELL)
+
+    # KINDS = (
+    #     ('b', 'Куплю'),
+    #     ('s', 'Продам'),
+    #     ('c', 'Обменяю'),
+    # )
+
+    # KINDS = (
+    #     ('Купля-продажа', (
+    #         ('b', 'Куплю'),
+    #         ('s', 'Продам'),
+    #     )),
+    #     ('Обмен', (
+    #         ('c', 'Обменяю'),
+    #     ))
+    # )
+
+    KINDS = (
+        (None, 'Выберите тип объявления'),
+        ('b', 'Куплю'),
+        ('s', 'Продам'),
+        ('c', 'Обменяю'),
+    )
+
+    # kind = models.CharField(max_length=1, choices=KINDS, default='s')
+    kind = models.CharField(max_length=1, choices=KINDS, blank=True)
+
     rubric = models.ForeignKey("Rubric", null=True, on_delete=models.PROTECT, verbose_name="Рубрика")
     title = models.CharField(max_length=50, verbose_name="Товар")
     content = models.TextField(null=True, blank=True, verbose_name="Описание")
-    price = models.FloatField(null=True, blank=True, verbose_name="Цена")
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Цена", default=0.0)
+    is_activate = models.BooleanField(default=is_active_default)
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name="Опубликовано")
+    updated = models.DateTimeField(auto_now=True, db_index=True, verbose_name="Изменено")
 
     def __str__(self):
         return f'{self.title}'
