2db8c0b6a5e4e0ef63bfc2bf9089d22873ba87f0
Бахытжан Бекарыс <@step.kz>
2023-11-28 21:17:03 +0600

    lesson_07 files

--- bboard/models.py
+++ bboard/models.py
@@ -1,3 +1,5 @@
+from django.core import validators
+from django.core.exceptions import ValidationError
 from django.db import models
 
 is_all_posts_passive = True
@@ -7,11 +9,38 @@ def is_active_default():
     return is_all_posts_passive
 
 
+def validate_even(value):
+    if value % 2 != 0:
+        raise ValidationError('Число %(value)s нечетное',
+                              code='odd',
+                              params={'value': value})
+
+class MinMaxValueValidator:
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def __call__(self, value):
+        if value < self.min_value or value > self.max_value:
+            raise ValidationError('Введенное число должно находиться в диапазоне от %(min)s до %(max)s',
+                                  code='out_of_range',
+                                  params={'min': self.min_value, 'max': self.max_value})
+
+
 class Rubric(models.Model):
     name = models.CharField(max_length=20, db_index=True, verbose_name="Название", unique=True)
 
     def __str__(self):
         return f'{self.name}'
+
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
+
+    # def delete(self, *args, **kwargs):
+    #     super().delete(self, *args, **kwargs)
+
+    def get_absolute_url(self):
+        return f"/{self.pk}/"
 
     class Meta:
         verbose_name_plural = 'Рубрики'
@@ -42,26 +71,59 @@ class Bb(models.Model):
     #     ))
     # )
 
-    KINDS = (
-        (None, 'Выберите тип объявления'),
-        ('b', 'Куплю'),
-        ('s', 'Продам'),
-        ('c', 'Обменяю'),
-    )
+    # KINDS = (
+    #     (None, 'Выберите тип объявления'),
+    #     ('b', 'Куплю'),
+    #     ('s', 'Продам'),
+    #     ('c', 'Обменяю'),
+    # )
 
     # kind = models.CharField(max_length=1, choices=KINDS, default='s')
-    kind = models.CharField(max_length=1, choices=KINDS, blank=True)
+    # kind = models.CharField(max_length=1, choices=KINDS, blank=True)
 
     rubric = models.ForeignKey("Rubric", null=True, on_delete=models.PROTECT, verbose_name="Рубрика")
-    title = models.CharField(max_length=50, verbose_name="Товар")
+    title = models.CharField(
+        max_length=50,
+        verbose_name="Товар",
+        validators=[validators.RegexValidator(regex='^.{4,}$')],
+        error_messages={'invalid': 'ERROR'}
+        # validators=[validators.MinLengthValidator(4),
+        #             validators.MinLengthValidator(50)]
+    )
     content = models.TextField(null=True, blank=True, verbose_name="Описание")
-    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Цена", default=0.0)
+    price = models.DecimalField(max_digits=8,
+                                decimal_places=2,
+                                verbose_name="Цена",
+                                default=0.0,
+                                # validators = [validators.MinValueValidator(0),
+                                #               validators.MinValueValidator(100500),
+                                #               validators.DecimalValidator(8, 2)]
+                                validators=[validate_even, MinMaxValueValidator(25, 45)])
     is_activate = models.BooleanField(default=is_active_default)
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name="Опубликовано")
     updated = models.DateTimeField(auto_now=True, db_index=True, verbose_name="Изменено")
 
     def __str__(self):
         return f'{self.title}'
+
+    def clean(self):
+        errors = {}
+        if not self.content:
+            errors['content'] = ValidationError('Укажите описание продаваемого товара')
+        if self.price and self.price < 0:
+            errors['price'] = ValidationError('Укажите корректное значение цены товара')
+        if errors:
+            raise ValidationError(errors)
+
+    def title_and_price(self):
+        if self.price:
+            return f'{self.title} ({self.price:.2f})'
+        return self.title
+
+    def id_and_date(self):
+        return f'{self.published}, id: {self.pk}'
+
+    # title_and_price.short_description = "Название и цена"
 
     class Meta:
         verbose_name_plural = 'Объявления'
