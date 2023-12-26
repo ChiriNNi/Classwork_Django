38a5663e78f9cc8061c1cd75314c973ab01fe985
Бахытжан Бекарыс <@step.kz>
2023-12-26 21:13:05 +0600

    lesson_15 files

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.core.serializers.json import DjangoJSONEncoder
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.http import StreamingHttpResponse, FileResponse, JsonResponse
+from django.urls import resolve
+
+from bboard.models import Rubric, Bb
+
+
+# def index(request):
+#     resp_content = ('Здесь будет', ' главная', ' страница', ' сайта')
+#     resp = StreamingHttpResponse(resp_content, content_type='text/plain; charset=utf-8')
+#     return resp
+
+# def index(request):
+#     file_name = r'static/bg.jpg'
+#     return FileResponse(open(file_name, 'rb'), as_attachment=True, filename='photo.jpg')
+
+
+# def index(request):
+#     data = {'title': 'Мотоцикл', 'content': 'Старый', 'price': 10000.0}
+#     return JsonResponse(data, encoder=DjangoJSONEncoder)
+
+# def index(request):
+#     r = get_object_or_404(Rubric, name='Транспорт')
+#     return redirect('bboard:by_rubric', rubric_id=r.id)
+
+
+# def index(request):
+#     r = get_object_or_404(Rubric, name='Транспорт')
+#     bbs = get_list_or_404(Bb, rubric=r)
+#     context={'bbs': bbs}
+#     res = resolve('/test/')
+#     return render(request, 'test.html', context)
