5e7552ecaa91411c5a31b6f02fdb12a70d41c958
Бахытжан Бекарыс <@step.kz>
2024-02-13 21:07:00 +0600

    lesson_28 homework files

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
@@ -27,6 +29,43 @@ class Machine(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class MachinePrice(Machine):
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def get_total_price(self):
+        total_price = self.price + sum(kit.spare.price for kit in self.kit_set.all())
+        return total_price
+
+
+class MachineMaintenance(MachinePrice):
+    def create_maintenance_schedule(self, start_date=None):
+        if start_date is None:
+            start_date = datetime.now().date()
+
+        price = self.price
+
+        if price <= 1000:
+            interval_in_months = 12
+        elif 1000 < price <= 5000:
+            interval_in_months = 6
+        else:
+            interval_in_months = 3
+
+        maintenance_dates = []
+        maintenance_dates.append(start_date)
+
+        for i in range(1, 12):
+            next_date = start_date + timedelta(days=i * 30 * interval_in_months)
+            maintenance_dates.append(next_date)
+
+        schedule_text = ', '.join(date.strftime('%Y-%m-%d') for date in maintenance_dates)
+
+        self.maintenance_schedule = schedule_text
+        self.save()
+
+        return schedule_text
 
 
 class Kit(models.Model):
